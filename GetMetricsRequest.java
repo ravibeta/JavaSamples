ba71b679a0d8724a69bbab1da75f669dbe37878f
Ravi Rajamani <ravibeta@hotmail.com>
2018-12-30 17:47:24 -0800

    Update GetMetricsRequest.java

--- GetMetricsRequest.java
+++ GetMetricsRequest.java
@@ -1,5 +1,9 @@
-package com.emc.ecs.s3.sample;
-import java.io.UnsupportedEncodingException;
+/*
+package com.emc.ecs.monitoring.sample;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import javax.net.ssl.HttpsURLConnection;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -12,43 +16,6 @@ import org.slf4j.LoggerFactory;
 public class GetMetricsRequest {
     private static final Logger logger = LoggerFactory.getLogger(GetMetricsRequest.class);
 
-    public static byte[] computeHmacSHA256(byte[] key, String data) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException,
-            UnsupportedEncodingException {
-        String algorithm = "HmacSHA256";
-        String charsetName = "UTF-8";
-
-        Mac sha256_HMAC = Mac.getInstance(algorithm);
-        SecretKeySpec secret_key = new SecretKeySpec(key, algorithm);
-        sha256_HMAC.init(secret_key);
-
-        return sha256_HMAC.doFinal(data.getBytes(charsetName));
-    }
-
-    public static byte[] computeHmacSHA256(String key, String data) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException,
-            UnsupportedEncodingException {
-        return computeHmacSHA256(key.getBytes(), data);
-    }
-
-    public static String getSignatureV4(String accessSecretKey, String date, String region, String regionService, String signing, String stringToSign)
-            throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
-
-        byte[] dateKey = computeHmacSHA256(accessSecretKey, date);
-        logger.debug("dateKey: {}", encodeToString(dateKey));
-
-        byte[] dateRegionKey = computeHmacSHA256(dateKey, region);
-        logger.debug("dateRegionKey: {}", encodeToString(dateRegionKey));
-
-        byte[] dateRegionServiceKey = computeHmacSHA256(dateRegionKey, regionService);
-        logger.debug("dateRegionServiceKey: {}", encodeToString(dateRegionServiceKey));
-
-        byte[] signingKey = computeHmacSHA256(dateRegionServiceKey, signing);
-        logger.debug("signingKey: {}", encodeToString(signingKey));
-
-        byte[] signature = computeHmacSHA256(signingKey, stringToSign);
-        logger.debug("signature: {}", encodeToString(signature));
-
-        return encodeToString(signature);
-    }
     private static final char[] DIGITS = {
             '0', '1', '2', '3', '4', '5', '6', '7',
             '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
@@ -94,9 +61,50 @@ public class GetMetricsRequest {
         return kSigning;
     }
 
+    protected static Map<String, String> getHeaders(String amz_date, String authorization_header, String apiName, String content_type) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("x-amz-date", amz_date);
+        headers.put("Authorization", authorization_header);
+        headers.put("x-amz-target", "GraniteServiceVersion20100801."+apiName);
+        headers.put("Content-Type", content_type);
+        headers.put("Accept", "application/json");
+        headers.put("Content-Encoding", "amz-1.0");
+        headers.put("Connection", "keep-alive");
+        return headers;
+    }
+
+
+    public static String getResponse(String httpsURL, Map<String, String> headers, String payload) throws Exception {
+            URL myurl = new URL(httpsURL);
+            String response = null;
+            logger.info("Sending a post request to:"  + httpsURL);
+            HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+            con.setRequestMethod("POST");
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                con.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            try (DataOutputStream output = new DataOutputStream(con.getOutputStream())) {
+                output.writeBytes(payload);
+            }
+            try (DataInputStream input = new DataInputStream(con.getInputStream())) {
+                StringBuffer contents = new StringBuffer();
+                String tmp;
+                while ((tmp = input.readLine()) != null) {
+                    contents.append(tmp);
+                    logger.debug("tmp="+tmp);
+                }
+                response = contents.toString();
+            }
+            logger.info("Resp Code:" + con.getResponseCode());
+            logger.info("Resp Message:" + con.getResponseMessage());
+            return response;
+        }
+
     public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
-        String AWS_ACCESS_KEY_ID="MY_ACCESS_KEY";
-        String AWS_SECRET_ACCESS_KEY="MY_SECRET_KEY";
+        String AWS_ACCESS_KEY_ID="MyAccessKey";
+        String AWS_SECRET_ACCESS_KEY="MyAccessSecret";
         String service="monitoring";
         String host="monitoring.us-east-1.amazonaws.com";
         String region="us-east-1";
@@ -112,10 +120,10 @@ public class GetMetricsRequest {
         String amz_target = "GraniteServiceVersion20100801."+apiName;
         String canonical_headers = "content-type:" + content_type + "\n" + "host:" + host + "\n" + "x-amz-date:" + amz_date + "\n" + "x-amz-target:" + amz_target + "\n";
         String signed_headers = "content-type;host;x-amz-date;x-amz-target";
-          String accessKey = AWS_ACCESS_KEY_ID;
-          String accessSecretKey = AWS_SECRET_ACCESS_KEY;
-          String date = "20130806";
-          String signing = "aws4_request";
+        String accessKey = AWS_ACCESS_KEY_ID;
+        String accessSecretKey = AWS_SECRET_ACCESS_KEY;
+        String date = "20130806";
+        String signing = "aws4_request";
         String request_parameters = "{";
         request_parameters += "    \"Action\": \"GetMetricStatistics\", ";
         request_parameters += "    \"Namespace\": \"On-PremiseObjectStorageMetrics\",";
@@ -135,25 +143,30 @@ public class GetMetricsRequest {
         request_parameters += "    \"Unit\": \"Bytes\"";
         request_parameters += "}";
 
-        // String payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
-        // String canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash;
-        // String algorithm = 'AWS4-HMAC-SHA256';
-        // String credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request';
-        // String string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest();
-        // String signing_key = getSignatureKey(secret_key, date_stamp, region, service);
-        // String signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest();
-        // String authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature;
-        // logger.info("signature: {}", getSignatureV4(accessSecretKey, date, region, regionService, signing, request_parameters));
         try {
+            String payload_hash = encodeToString(HmacSHA256(request_parameters, accessSecretKey.getBytes())); // hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
+            String canonical_request = method + "\n" + canonical_uri + "\n" + canonical_querystring + "\n" + canonical_headers + "\n" + signed_headers + "\n" + payload_hash;
+            String algorithm = "AWS4-HMAC-SHA256";
+            String credential_scope = date_stamp + "/" + region + "/" + service + "/" + "aws4_request";
+            String string_to_sign = algorithm + "\n" +  amz_date + "\n" +  credential_scope + "\n" +  encodeToString(HmacSHA256(canonical_request, accessSecretKey.getBytes()));
+            // logger.info("signature: {}", getSignatureV4(accessSecretKey, date, region, regionService, signing, request_parameters));
             byte[] signing_key = getSignatureKey(accessSecretKey, date_stamp, region, service);
+            String signature = encodeToString(HmacSHA256(string_to_sign, signing_key));
             logger.info("signature: {}", encodeToString(signing_key));
+            String authorization_header = algorithm + " " + "Credential=" + accessKey + "/" + credential_scope + ", " +  "SignedHeaders=" + signed_headers + ", " + "Signature=" + signature;
+            logger.info("authorization_header="+authorization_header);
+            Map<String, String> headers= getHeaders(amz_date, authorization_header, apiName, content_type);
+            logger.info("Sending request with:" + request_parameters);
+            String response = getResponse(endpoint, headers, request_parameters);
+            logger.info("response:"+response);
         } catch (Exception e) {
             e.printStackTrace();
+            logger.error("Exception:", e);
         }
     }
-
 }
 //
 // output:
 // [main] INFO com.emc.ecs.s3.sample.GetMetricsRequest - signature: c1391d813f0596e30497d180105f3e2a0defd24f4c5d15d0bdfa22dc905f7e42
-//
+//package com.emc.ecs.monitoring.sample;
+*/
