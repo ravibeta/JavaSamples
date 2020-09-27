fc1e9ea1a74903cbcd21e1698ba7cee39348530c
ravibeta <ravibeta@hotmail.com>
2020-09-27 02:42:06 +0000

    StripeApp

--- StripeApp/src/main/java/com/stripe/app/StripeApp.java
+++ StripeApp/src/main/java/com/stripe/app/StripeApp.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class StripeApp {
     private static final Logger logger = LoggerFactory.getLogger(StripeApp.class);
-    private static String apiPath = "v1/payment-intents/";
+    private static String apiPath = "v1/payment_intents";
     private static String httpsURL = "https://api.stripe.com/";
     private static String clientId = Constants.DEFAULT_PUBLISHABLE;
     private static String clientSecret = Constants.DEFAULT_SECRET + ":";
@@ -27,7 +27,7 @@ public class StripeApp {
     con.setRequestMethod("GET");
     con.setRequestProperty("Content-Type","application/x-www- form-urlencoded"); 
     con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)"); 
-    con.setRequestProperty("Authentication", "Bearer " + Base64.getEncoder().encodeToString(new String(clientSecret+":").getBytes()));
+    con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString(new String(clientSecret+":").getBytes()));
     con.setRequestProperty("Stripe-Account", accountId);
     con.setDoInput(true); 
     DataInputStream input = new DataInputStream( con.getInputStream() ); 
@@ -42,9 +42,15 @@ public class StripeApp {
 }
 
 /*
-Exception in thread "main" java.io.IOException: Server returned HTTP response code: 401 for URL: https://api.stripe.com/v1/payment-intents/
-        at java.base/sun.net.www.protocol.http.HttpURLConnection.getInputStream0(HttpURLConnection.java:1924)
-        at java.base/sun.net.www.protocol.http.HttpURLConnection.getInputStream(HttpURLConnection.java:1520)
-        at java.base/sun.net.www.protocol.https.HttpsURLConnectionImpl.getInputStream(HttpsURLConnectionImpl.java:250)
-        at com.stripe.app.StripeApp.main(StripeApp.java:33)
+java -jar build//libs/StripeApp-1.0.jar
+{
+  "object": "list",
+  "data": [
+
+  ],
+  "has_more": false,
+  "url": "/v1/payment_intents"
+}
+Resp Code:200
+Resp Message:OK
 */
