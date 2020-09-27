fe0b910a2029a164d0415484768f84e847240aac
ravibeta <ravibeta@hotmail.com>
2020-09-27 02:07:26 +0000

    add call

--- StripeApp/src/main/java/com/stripe/app/Constants.java
+++ StripeApp/src/main/java/com/stripe/app/Constants.java
@@ -2,5 +2,6 @@ package com.stripe.app;
 public class Constants {
     protected static final String DEFAULT_PUBLISHABLE = "pk_test_51HVmXmCUt3t8PcFNQX0uBift2rFvuRe7BCqQiaO8vbdLXJSXz0AQk93p4CrqborjZQOc0DIPDdqw307Ka0FgrSv300eegZhJU8";
     protected static final String DEFAULT_SECRET = "sk_test_51HVmXmCUt3t8PcFNUT4BXnvVzSCR0sVRz0e4cbZHBTGRt4V2a6t6HGj7YyhoudaYkGX1CGkJDPJhZc6StABwBj6L00WFUdr8n2";
+    protected static final String DEFAULT_ACCOUNT = "acct_1HVmXmCUt3t8PcFN";
 }
 
--- StripeApp/src/main/java/com/stripe/app/StripeApp.java
+++ StripeApp/src/main/java/com/stripe/app/StripeApp.java
@@ -1,16 +1,55 @@
 
-package com.Stripe.app;
+package com.stripe.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.net.*;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class StripeApp {
     private static final Logger logger = LoggerFactory.getLogger(StripeApp.class);
+    private static String apiPath = "v1/payment-intents/";
+    private static String httpsURL = "https://api.stripe.com";
+    private static String clientId = Constants.DEFAULT_PUBLISHABLE;
+    private static String clientSecret = Constants.DEFAULT_SECRET;
+    private static String accountId = Constants.DEFAULT_ACCOUNT;
 
-    public static void main(String argv[]) throws Exception {
-    }
+
+    public static void main(String[] args) throws Exception {
+    URL myurl = new URL(httpsURL+apiPath);
+    HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Content-Type","application/x-www- form-urlencoded"); 
+    con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)"); 
+    con.setRequestProperty("Authentication", "Bearer " + clientSecret);
+    con.setRequestProperty("Stripe-Account", accountId);
+    con.setDoOutput(true); 
+    con.setDoInput(true); 
+
+
+    DataOutputStream output = new DataOutputStream(con.getOutputStream());  
+
+
+    output.writeBytes(query);
+
+    output.close();
+
+    DataInputStream input = new DataInputStream( con.getInputStream() ); 
+
+
+
+    for( int c = input.read(); c != -1; c = input.read() ) 
+    System.out.print( (char)c ); 
+    input.close(); 
+
+    System.out.println("Resp Code:"+con .getResponseCode()); 
+    System.out.println("Resp Message:"+ con .getResponseMessage()); 
+   }
+
 }
 
